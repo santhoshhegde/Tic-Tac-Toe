b39ae09a27a4e2bc8540e291721b636f1635bb7a
Santhosh Hegde <santhosh.hegde262000@gmail.com>
2024-01-30 20:01:07 +0530

    Buttons innerText logic

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+let boxes = document.querySelectorAll(".box");
+let isTrunO = true;
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(isTrunO){
+            box.innerText = "O";
+            isTrunO = false;
+        }
+        else{
+            box.innerText = "X";
+            isTrunO = true;
+        }
+    })
+});
